b5d2356a1ce60df19f9380d0aa2317fd93589640
nfaciano <nickfac88@gmail.com>
2024-03-23 16:42:53 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 1337;
+
+function serveStaticFile(res, filePath, contentType, responseCode = 200) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        fs.readFile('./public/404.html', (error, content) => {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        });
+      } else {
+        res.writeHead(500);
+        res.end('Internal Server Error');
+      }
+    } else {
+      res.writeHead(responseCode, { 'Content-Type': contentType });
+      res.end(data, 'utf-8');
+    }
+  });
+}
+
+http.createServer((req, res) => {
+  let filePath = './public' + req.url;
+  if (filePath == './public/') {
+    filePath = './public/index.html';
+  }
+
+  let extname = String(path.extname(filePath)).toLowerCase();
+  let mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.svg': 'image/svg+xml',
+    '.xml': 'application/xml'
+    // Add more MIME types if needed
+  };
+
+  let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  serveStaticFile(res, filePath, contentType);
+}).listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
