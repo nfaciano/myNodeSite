d42ebec6eb6213be1e6b7e9c5ccb2d1810c2cec0
nfaciano <nickfac88@gmail.com>
2024-03-24 12:32:29 -0400

    fixed links

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
+const https = require('https');
 
 const port = 1337;
 
@@ -31,6 +32,28 @@ function serveStaticFile(res, filePath, contentType, responseCode = 200) {
   });
 }
 
+function fetchAndServeXml(res, url) {
+  https.get(url, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      res.writeHead(200, {
+        'Content-Type': 'application/xml',
+        'Access-Control-Allow-Origin': 'https://lambdachiuri-node.onrender.com'
+      });
+      res.end(data);
+    });
+  }).on('error', (error) => {
+    console.error(`Error fetching XML file: ${error.message}`);
+    res.writeHead(500);
+    res.end('Internal Server Error');
+  });
+}
+
 http.createServer((req, res) => {
   // Use CORS middleware
   corsMiddleware(req, res, () => {
@@ -39,22 +62,27 @@ http.createServer((req, res) => {
       filePath = './public/index.html';
     }
 
-    let extname = String(path.extname(filePath)).toLowerCase();
-    let mimeTypes = {
-      '.html': 'text/html',
-      '.js': 'text/javascript',
-      '.css': 'text/css',
-      '.json': 'application/json',
-      '.png': 'image/png',
-      '.jpg': 'image/jpeg',
-      '.svg': 'image/svg+xml',
-      '.xml': 'application/xml'
-      // Add more MIME types if needed
-    };
+    if (filePath.endsWith('.xml')) {
+      const githubRawUrl = 'https://raw.githubusercontent.com/nfaciano/myNodeSite/main/public/data/merchandise.xml';
+      fetchAndServeXml(res, githubRawUrl);
+    } else {
+      let extname = String(path.extname(filePath)).toLowerCase();
+      let mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.svg': 'image/svg+xml',
+        '.xml': 'application/xml'
+        // Add more MIME types if needed
+      };
 
-    let contentType = mimeTypes[extname] || 'application/octet-stream';
+      let contentType = mimeTypes[extname] || 'application/octet-stream';
 
-    serveStaticFile(res, filePath, contentType);
+      serveStaticFile(res, filePath, contentType);
+    }
   });
 }).listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
