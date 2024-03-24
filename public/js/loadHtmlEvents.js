ae2b267cd256003dee2f5895bf230b08e3d048e2
nfaciano <nickfac88@gmail.com>
2024-03-24 16:50:37 -0400

    final push

--- public/js/loadHtmlEvents.js
+++ public/js/loadHtmlEvents.js
@@ -1,5 +1,5 @@
 function loadHtmlEvents() {
-    $("#eventsList").load("/data/aboutus-content.html", function(response, status, xhr) {
+    $("#eventsList").load("/data/events-data.html", function(response, status, xhr) {
         if (status == "error") {
             var msg = "Sorry but there was an error: ";
             $("#eventsList").html(msg + xhr.status + " " + xhr.statusText);
