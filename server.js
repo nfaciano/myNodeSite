aa709b6eee3c6c6cddba862afef2cbd9f5abc268
nfaciano <nickfac88@gmail.com>
2024-03-24 12:29:58 -0400

    fixed links

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 const port = 1337;
+
+// Enable CORS middleware
+const corsOptions = {
+  origin: 'https://lambdachiuri-node.onrender.com'
+};
+
+const corsMiddleware = cors(corsOptions);
 
 function serveStaticFile(res, filePath, contentType, responseCode = 200) {
   fs.readFile(filePath, (err, data) => {
@@ -17,37 +25,37 @@ function serveStaticFile(res, filePath, contentType, responseCode = 200) {
         res.end('Internal Server Error');
       }
     } else {
-      res.writeHead(responseCode, {
-        'Content-Type': contentType,
-        'Access-Control-Allow-Origin': 'https://lambdachiuri-node.onrender.com'
-      });
+      res.writeHead(responseCode, { 'Content-Type': contentType });
       res.end(data, 'utf-8');
     }
   });
 }
 
 http.createServer((req, res) => {
-  let filePath = './public' + req.url;
-  if (filePath == './public/') {
-    filePath = './public/index.html';
-  }
+  // Use CORS middleware
+  corsMiddleware(req, res, () => {
+    let filePath = './public' + req.url;
+    if (filePath == './public/') {
+      filePath = './public/index.html';
+    }
 
-  let extname = String(path.extname(filePath)).toLowerCase();
-  let mimeTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.svg': 'image/svg+xml',
-    '.xml': 'application/xml'
-    // Add more MIME types if needed
-  };
+    let extname = String(path.extname(filePath)).toLowerCase();
+    let mimeTypes = {
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+      '.json': 'application/json',
+      '.png': 'image/png',
+      '.jpg': 'image/jpeg',
+      '.svg': 'image/svg+xml',
+      '.xml': 'application/xml'
+      // Add more MIME types if needed
+    };
 
-  let contentType = mimeTypes[extname] || 'application/octet-stream';
+    let contentType = mimeTypes[extname] || 'application/octet-stream';
 
-  serveStaticFile(res, filePath, contentType);
+    serveStaticFile(res, filePath, contentType);
+  });
 }).listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
